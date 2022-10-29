940bc6081d8e1d2285e067a1ceced84424d44647
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-28 19:42:27 -0700

    express app added, button redirects to notes html

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//Allows the express app to run
+app.use(express.static('public'));
+
 // Data
 
 // Routes
@@ -15,7 +18,7 @@ app.get('/', (req,res) =>
 );
 
 //GET route for note page
-app.get('/', (req,res) =>
+app.get('/notes', (req,res) =>
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 );
 
