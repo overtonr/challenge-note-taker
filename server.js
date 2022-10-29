73fa31124f5ea333e0c12c35e9f88ae072aea11e
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-28 19:25:19 -0700

    dependecies and listener added

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Data
+
+// Routes
+
+// Listener
+app.listen(PORT, () =>
+console.log(`Listening at  http://localhost:${PORT}`)
+);
+
