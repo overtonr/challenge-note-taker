62b9da2af192525c0062359ac8283e97cd4a677d
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-28 19:35:20 -0700

    get routes for html pages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +9,15 @@ const app = express();
 // Data
 
 // Routes
+//GET route for landing page
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname,'/public/index.html'))
+);
+
+//GET route for note page
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname,'/public/notes.html'))
+);
 
 // Listener
 app.listen(PORT, () =>
